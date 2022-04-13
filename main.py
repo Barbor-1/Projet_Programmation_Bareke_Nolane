a93bfd5aca3b1db524b07ec3e78ac6ba14ca2fbb
monsieur7 <nolane.de@gmail.com>
2022-04-13 20:21:21 +0200

    CODE AUTHENTIFICATION A ETE MIS DANS SON PROPRE FICHIER

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 # importing the requests library
+from socket import timeout
 import requests
 from requests import *
-import matplotlib.pyplot as plt
-URL = "https://digital.iservices.rte-france.com/token/oauth/ "
+#import matplotlib.pyplot as plt
 
-r = requests.post(URL, headers={"Authorization": "Basic ZDJhMWEyZDctN2E0My00MmQ2LWFhMWMtOThiN2IzMDM0N2NiOjk3MTJmNWRjLTEwZTAtNDRmZi05NDNjLWNhMmMxNTJhYjc1Mw=="})
+from network import RTEauth
+URL = "https://digital.iservices.rte-france.com/token/oauth/ "
+key = "ZDJhMWEyZDctN2E0My00MmQ2LWFhMWMtOThiN2IzMDM0N2NiOjk3MTJmNWRjLTEwZTAtNDRmZi05NDNjLWNhMmMxNTJhYjc1Mw=="
+"""r = requests.post(URL, headers={"Authorization": "Basic ZDJhMWEyZDctN2E0My00MmQ2LWFhMWMtOThiN2IzMDM0N2NiOjk3MTJmNWRjLTEwZTAtNDRmZi05NDNjLWNhMmMxNTJhYjc1Mw=="})
 print(r.json())
 print(r.json()['access_token'])
 token = r.json()['access_token']
+"""
+auth = RTEauth(key)
+token = auth.getApiKey()
+print(token)
 
 gen = requests.get("https://digital.iservices.rte-france.com/open_api/actual_generation/v1/actual_generations_per_production_type", headers={"Authorization": "Bearer "+ token})
 #print(gen.json())
@@ -15,3 +22,6 @@ gen = requests.get("https://digital.iservices.rte-france.com/open_api/actual_gen
 for i in range(0, len(gen.json()['actual_generations_per_production_type'])):
     for j in range(0, len(gen.json()['actual_generations_per_production_type'][i]['values'])):
         print(gen.json()['actual_generations_per_production_type'][i]['production_type'] + " " + str(gen.json()['actual_generations_per_production_type'][i]['values'][j]['value']) + " " + gen.json()['actual_generations_per_production_type'][i]['values'][j]['start_date'])
+
+
+
