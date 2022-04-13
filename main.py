0befe6d21be28eef4f401bb18b6bf5acf2b14082
monsieur7 <nolane.de@gmail.com>
2022-04-13 16:12:25 +0200

    CODE RESEAU-AUTH

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# importing the requests library
+import requests
+from requests import *
+URL = "https://digital.iservices.rte-france.com/token/oauth/ "
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+r = requests.post(URL, headers={"Authorization": "Basic ZDJhMWEyZDctN2E0My00MmQ2LWFhMWMtOThiN2IzMDM0N2NiOjk3MTJmNWRjLTEwZTAtNDRmZi05NDNjLWNhMmMxNTJhYjc1Mw=="})
+print(r.json())
+print(r.json()['access_token'])
+token = r.json()['access_token']
