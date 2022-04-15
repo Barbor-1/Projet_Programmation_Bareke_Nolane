63889d8481a93985e5e597c5a88499e285d96af9
monsieur7 <nolane.de@gmail.com>
2022-04-15 11:34:35 +0200

    SYSTEMES DE BOUTTONS + CARTE BACKEND

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import button
+from map_gen import map
 running = True
 
 pygame.init()
@@ -7,18 +8,25 @@ pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("RTS GAME - v1")
 
-button1 = button.Button(0, 0, (255, 255, 255),(255, 0, 0), screen)
+carte = map(screen, "carte.bmp")
+carte.display_map()
+
+"""button1 = button.Button(0, 0, (255, 255, 255),(255, 0, 0), screen)
 button1.setLabel("test")
 button1.drawButton()
+"""
+
+
 
 pygame.display.flip()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                 running = False
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+        """if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             pos = pygame.mouse.get_pos()
 
             if(button1.collide(pos) == 1):
                print("collided")
+        """
     
---
+++ map_gen.py
@@ -0,0 +1,9 @@
+import pygame
+
+class map:
+    def __init__(self, screen, image_path):
+        self.screen = screen
+        self.path = image_path
+    def display_map(self):
+        self.image =  pygame.image.load(self.path)
+        self.screen.blit(self.image, (0, 0))
