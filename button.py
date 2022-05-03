0d697c2b467c7d962388a1554c3fd5a1caffecc8
Baréké <n.bareke@gmail.com>
2022-05-03 16:57:44 +0200

    PARTIAL FIX BOUTON

--- button.py
+++ button.py
@@ -10,12 +10,12 @@ class Button:
         self.background_color = background_color
     def setLabel(self, label):
         self.label = label
-    def drawButton(self):
+    def initButton(self):
         smallfont = pygame.font.SysFont('Corbel',self.size) # can change it ? 
         self.text = smallfont.render(self.label, True , self.color) # render font
         self.text_rect = self.text.get_rect(topleft=(self.x, self.y)) # get rectangle
+    def drawButton(self):
         pygame.draw.rect(self.screen, self.background_color, (self.text_rect.x, self.text_rect.y, self.text_rect.width, self.text_rect.height))
-
         self.screen.blit(self.text, self.text_rect) # blit it 
     def collide(self, pos):
         return self.text_rect.collidepoint(pos)
--- main.py
+++ main.py
@@ -15,19 +15,21 @@ screen_object = Screen("RTS GAME - v1-MENU", 800, 800)
 screen_object.makeCurrent()
 screen = screen_object.getScreen()
 
-change_screen = Screen("RTS GAME - v1")
+change_screen = Screen("RTS GAME - v1", 800, 800)
 screen2 = change_screen.getScreen()
 
 
-carte = map(screen, os.path.join(os.getcwd(), "Test_carte.png")) #IMPORTANTx
+carte = map(screen, os.path.join(os.getcwd(), "Test_carte.png")) #IMPORTANTx. B-Currently the background is placeholder
 carte.display_map()
 
 button1 = button.Button(10, 10, (255, 255, 255),(255, 0, 0), screen)
 button1.setLabel("test")
+button1.initButton()
 button1.drawButton()
 
-button2 = button.Button(500, 10, (255, 255, 255),(150, 63, 29), screen2)
+button2 = button.Button(500, 10, (0, 0, 0),(235, 125, 56), screen2)
 button2.setLabel("go back")
+button2.initButton()#Initialise values for example text_rect that could crash when we click the screen in the while
 
 pygame.display.flip() # update display (IMPORTANT)
 while running:
@@ -37,16 +39,18 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: # put it inside class with event as argument ?| event.button == 1 : left click
             pos = pygame.mouse.get_pos()
 
-            if(button1.collide(pos) == 1):   
+            if(button1.collide(pos) == 1):
+                #B-Still able to press button 1 even if fliped, flashes white when pressed
                print("collided 1")
                fliped = True
                screen_object.endCurrent() #do nothing, see later
                change_screen.makeCurrent() # change screen + update screen => (maybe remove it dont know ?)
-               pygame.draw.rect(screen2, (0, 255, 0), (0, 0, 200, 200))
+               pygame.draw.rect(screen2, (75, 63, 143), (300, 300, 200, 200))
                button2.drawButton()
                pygame.display.flip()
 
             if(button2.collide(pos) == 1):
+                #B-Still able to press button 2 even if fliped
                 print("collided 2")
                 fliped = False
                 change_screen.endCurrent() #do nothing, see later
