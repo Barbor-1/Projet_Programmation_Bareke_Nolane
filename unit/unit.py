afc5cbb525c5338ab5ff506f97ebdc6c51fed1d0
monsieur7 <nolane.de@gmail.com>
2022-05-07 13:27:50 +0200

    UNIT ID

--- unit/unit.py
+++ unit/unit.py
@@ -6,7 +6,7 @@ class Unit():
     def __init__(self):
         self.pos_x = 0
         self.pos_y = 0
-        self.allegiance = 0  # A qui appartient l'unité. +1 pour un joueur -1 pour l'autre?
+        self.allegiance = 0  # A qui appartient l'unité. +1 pour un joueur -1 pour l'autre => ça peut passer
         self.type = ""
         self.health = 10
         self.atk = 5  # Valeur de puissance d'attaque
