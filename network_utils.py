dc537681a99a7d4e1330a0a40d72b7b6396969b3
monsieur7 <nolane.de@gmail.com>
2022-05-20 15:31:12 +0200

    bugfix : delete unit  (3)

--- network_utils.py
+++ network_utils.py
@@ -1,13 +1,11 @@
 def remove_unit(grid, id):
-    for i in range(0, len(grid.getGridSize())):
-        for j in range(0, len(grid.getGridSize())):
+    for i in range(0, grid.getGridSize()):
+        for j in range(0, grid.getGridSize()):
 
             unit = grid.getUnitAtGrid(i, j)
             if(unit != 0):
                 if(unit.id == id):
                     grid.deleteUnitAtGrid(i, j)
-                    print("new unit list", unit_list)
                     return unit_list
-            print("new unit list", unit_list)
             return unit_list
 
