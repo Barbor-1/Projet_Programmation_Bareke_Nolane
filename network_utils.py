5f6a51e2ef18bc8c960a0cfa98f36cd2bb9902a3
monsieur7 <nolane.de@gmail.com>
2022-05-20 15:28:09 +0200

    bugfix : delete unit + commentaires (2)

--- network_utils.py
+++ network_utils.py
@@ -2,12 +2,12 @@ def remove_unit(grid, id):
     for i in range(0, len(grid.getGridSize())):
         for j in range(0, len(grid.getGridSize())):
 
-        unit = grid.getUnitAtGrid(i, j)
-        if(unit != 0):
-            if(unit.id == id):
-                grid.deleteUnitAtGrid(i, j)
-                print("new unit list", unit_list)
-                return unit_list
-        print("new unit list", unit_list)
-        return unit_list
+            unit = grid.getUnitAtGrid(i, j)
+            if(unit != 0):
+                if(unit.id == id):
+                    grid.deleteUnitAtGrid(i, j)
+                    print("new unit list", unit_list)
+                    return unit_list
+            print("new unit list", unit_list)
+            return unit_list
 
