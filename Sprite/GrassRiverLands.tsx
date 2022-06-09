41524899542e9100facd17cf9a7afe313b3da66a
Baréké <n.bareke@gmail.com>
2022-06-09 11:00:24 +0200

    Updated game background

---
+++ Sprite/GrassRiverLands.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="GrassRiverLands" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="TILEMAP-1.png" width="256" height="256"/>
+ <wangsets>
+  <wangset name="Jeu de Tuiles Sans Nom" type="corner" tile="-1">
+   <wangcolor name="River" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Corner" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Center" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="Leftright" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="TopBot" color="#00e9ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="1" wangid="0,5,0,0,0,0,0,5"/>
+   <wangtile tileid="2" wangid="0,5,0,0,0,0,0,5"/>
+   <wangtile tileid="3" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="4" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="5" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="8" wangid="0,0,0,0,0,4,0,4"/>
+   <wangtile tileid="9" wangid="0,3,0,3,0,3,0,0"/>
+   <wangtile tileid="10" wangid="0,0,0,3,0,3,0,3"/>
+   <wangtile tileid="11" wangid="0,4,0,4,0,0,0,0"/>
+   <wangtile tileid="12" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="16" wangid="0,0,0,0,0,4,0,4"/>
+   <wangtile tileid="17" wangid="0,3,0,3,0,0,0,3"/>
+   <wangtile tileid="18" wangid="0,3,0,0,0,3,0,3"/>
+   <wangtile tileid="19" wangid="0,4,0,4,0,0,0,0"/>
+   <wangtile tileid="20" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="24" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="25" wangid="0,0,0,5,0,5,0,0"/>
+   <wangtile tileid="26" wangid="0,0,0,5,0,5,0,0"/>
+   <wangtile tileid="27" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="28" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="29" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="36" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="37" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="44" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="45" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="52" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="53" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="60" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="61" wangid="0,1,0,0,0,0,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
